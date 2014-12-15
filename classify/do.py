f551780c4e827868113722c0c2fc4387873fe1b8
Alfredo Cobo <ajscobo@gmail.com>
2014-12-15 12:32:08 -0300

    Update do.py

--- classify/do.py
+++ classify/do.py
@@ -157,7 +157,7 @@ parameters =  {
             dict(
                 classifier__C=[2**x for x in range(-5,15)],
                 classifier__penalty=['l1', 'l2'],
-                classifier__tol=[1e-02,1e-03,1e-04,1e-05],
+                classifier__tol=[1e-02,1e-03,1e-04],
                 classifier__fit_intercept=[True, False],
                 classifier__intercept_scaling=[1,5,10,50,100,500,1000]
             )
@@ -181,7 +181,7 @@ parameters =  {
                 classifier__probability=[True],
                 classifier__shrinking=[True, False],
                 #classifier__dual=[False],
-                classifier__tol=[1e-02,1e-03,1e-04,1e-05],
+                classifier__tol=[1e-02,1e-03,1e-04],
             )
         ],
         'SVM RBF': [
@@ -192,7 +192,7 @@ parameters =  {
                 classifier__probability=[True],
                 classifier__shrinking=[True, False],
                 #classifier__dual=[False],
-                classifier__tol=[1e-02,1e-03,1e-04,1e-05],
+                classifier__tol=[1e-02,1e-03,1e-04],
             )
         ],
         'SVM Linear': [
@@ -203,7 +203,7 @@ parameters =  {
                 classifier__penalty=['l1'],
                 #Just L2, because l1 and l1 is ot permitted
                 #classifier__dual=[False],
-                classifier__tol=[1e-02,1e-03,1e-04,1e-05],
+                classifier__tol=[1e-02,1e-03,1e-04],
                 classifier__fit_intercept=[True, False],
                 classifier__intercept_scaling=[1,5,10,50,100,500,1000]
             )
@@ -227,15 +227,15 @@ parameters =  {
             dict(
                  classifier__loss=['hinge', 'log', 'modified_huber', 'squared_hinge', 'perceptron', 'squared_loss', 'huber', 'epsilon_insensitive', 'squared_epsilon_insensitive'],
                  classifier__penalty=['l1', 'l2', 'elasticnet'],
-                 classifier__alpha=[0.01, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.4, 0.5, 0.6, 0.8, 0.9, 1],
-                 classifier__l1_ratio=[0.01, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.4, 0.5, 0.6, 0.8, 0.9, 1],
+                 classifier__alpha=[0.01, 0.1, 0.5, 1],
+                 classifier__l1_ratio=[0.01, 0.1, 0.5, 1],
                  classifier__fit_intercept=[True, False],
-                 classifier__n_iter=[3,4,5,6,7],
+                 classifier__n_iter=[4,5,6],
                  classifier__shuffle=[True, False],
-                 classifier__epsilon=[1e-01, 1e-02,1e-03,1e-04,1e-05],
+                 classifier__epsilon=[1e-01, 1e-02,1e-03,1e-04],
                  #classifier__learning_rate default is optimal
                  #classifier__eta0
-                 classifier__power_t=[0.01, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.4, 0.5, 0.6, 0.8, 0.9, 1]
+                 classifier__power_t=[0.01, 0.1, 0.5, 1]
             )
         ]
 }
